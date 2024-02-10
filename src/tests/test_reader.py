67508d0fdf2a9b99029b1cfa510e9dbae7168007
Bogdan <bogdan.polygalov@gmail.com>
2024-02-10 16:21:56 +0500

    Ещё тесты

--- src/tests/test_reader.py
+++ src/tests/test_reader.py
@@ -1,3 +1,66 @@
 """
 Тестирование функций поиска (чтения) собранной информации в файлах.
 """
+
+
+import pytest
+from collectors.models import (
+    CountryDTO,
+    LocationDTO,
+    LocationInfoDTO,
+    NewsInfoDTO,
+    WeatherInfoDTO,
+)
+from reader import Reader
+
+
+@pytest.mark.asyncio
+class TestReader:
+    location = LocationDTO(
+        alpha2code="RU",
+        capital="Moscow",
+    )
+
+    @pytest.fixture
+    def reader(self):
+        return Reader()
+
+    async def test_find(self, reader):
+        location = await reader.find("Russia")
+        assert type(location) == LocationInfoDTO
+        assert location.location.name == "Russian Federation"
+        assert location.location.capital == "Moscow"
+        assert location.location.alpha2code == "RU"
+        assert len(location.location.currencies) == 1
+        assert len(location.location.languages) == 1
+        assert len(location.location.timezones) == 9
+        assert location.location.population > 145934462
+        assert location.location.area == 17124442
+        assert location.location.longitude == 100.0
+        assert location.location.latitude == 60.0
+        assert len(location.location.alt_spellings) == 5
+        assert location.location.subregion == "Eastern Europe"
+        assert type(location.weather) == WeatherInfoDTO
+        assert location.weather.timezone == 3
+        assert len(location.news) == 3
+        assert len(location.currency_rates) == 1
+
+    async def test_get_weather(self, mocker, reader):
+        weather = await reader.get_weather(self.location)
+        assert type(weather) == WeatherInfoDTO
+        assert weather.timezone == 3
+
+    async def test_get_news(self, mocker, reader):
+        news = await reader.get_news(self.location)
+        assert len(news) == 3
+        assert type(news[0]) == NewsInfoDTO
+
+    async def test_find_country(self, mocker, reader):
+        country = await reader.find_country("Russia")
+        assert type(country) == CountryDTO
+        assert country.name == "Russian Federation"
+        assert country.capital == "Moscow"
+
+    async def test_find_country_none(self, mocker, reader):
+        country = await reader.find_country("test")
+        assert country is None
--- src/tests/test_renderer.py
+++ src/tests/test_renderer.py
@@ -1,3 +1,118 @@
 """
 Тестирование функций генерации выходных данных.
 """
+
+
+import pytest
+from collectors.models import (
+    CountryDTO,
+    CurrencyInfoDTO,
+    LanguagesInfoDTO,
+    LocationInfoDTO,
+    NewsInfoDTO,
+    WeatherInfoDTO,
+)
+from renderer import Renderer
+
+
+@pytest.mark.asyncio
+class TestRenderer:
+    location = LocationInfoDTO(
+        location=CountryDTO(
+            alpha2code="RU",
+            capital="Moscow",
+            currencies={CurrencyInfoDTO(code="USD")},
+            languages={LanguagesInfoDTO(name="Russian", native_name="Русский")},
+            flag="test",
+            subregion="test",
+            name="Russia",
+            population=3,
+            area=3,
+            longitude=3,
+            latitude=3,
+            alt_spellings=["test"],
+            timezones=[3],
+        ),
+        weather=WeatherInfoDTO(
+            timezone=3,
+            temp=3,
+            pressure=3,
+            humidity=3,
+            wind_speed=3,
+            visibility=3,
+            dt=1,
+            description="test",
+        ),
+        currency_rates={"USD": 1.0},
+        news=[
+            NewsInfoDTO(
+                source="test",
+                published_at=0,
+                title="test",
+                content="test",
+                description="test",
+                url="test",
+                url_to_image="test",
+            )
+        ],
+    )
+
+    async def test_format_languages(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_languages()
+        assert result == "Russian (Русский)"
+
+    async def test_format_currencies_rates(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_currency_rates()
+        assert result == "USD = 1.00 руб."
+
+    async def test_format_news(self):
+        renderer = Renderer(self.location)
+        results = await renderer._format_news(self.location.news, 10, 20)
+        assert len(results) == 7
+        first_column = [
+            "Источник",
+            "Новость",
+            "Ссылка",
+            "Дата",
+            "Описание",
+            "Текст",
+            "",
+        ]
+        second_column = [
+            "test",
+            "test",
+            "test",
+            "01.01.197000:00",
+            "test",
+            "test",
+            None,
+        ]
+        for result, first_col, second_col in zip(results, first_column, second_column):
+            if result[0] == "-":
+                continue
+            result = result.replace(" ", "").split("|")
+            assert result[1] == first_col, f"{result[1]} != {first_col}"
+            assert result[2] == second_col, f"{result[2]} != {second_col}"
+
+    async def test_format_population(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_population()
+        assert result == "3"
+
+    async def test_format_news_line(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_news_line("test", "test", 10, 10)
+        assert result == ["|test      |      test|"]
+
+    async def test_format_news_line_long(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_news_line("test", "test" * 10, 10, 10)
+        assert len(result) == 4
+        assert result == [
+            "|test      |testtestte|",
+            "|          |sttesttest|",
+            "|          |testtestte|",
+            "|          |sttesttest|",
+        ]
