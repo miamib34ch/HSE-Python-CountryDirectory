b8d11766ef568e6a6d721c40fb18542afbc2ee81
Bogdan <bogdan.polygalov@gmail.com>
2024-02-10 14:25:10 +0500

    Читатель информации

--- src/reader.py
+++ src/reader.py
@@ -9,6 +9,7 @@ from collectors.collector import (
     CountryCollector,
     CurrencyRatesCollector,
     WeatherCollector,
+    NewsCollector,
 )
 from collectors.models import (
     CountryDTO,
@@ -16,6 +17,7 @@ from collectors.models import (
     LocationDTO,
     LocationInfoDTO,
     WeatherInfoDTO,
+    NewsInfoDTO,
 )
 
 
@@ -33,16 +35,21 @@ class Reader:
         """
 
         country = await self.find_country(location)
+        location_dto = LocationDTO(
+            capital=country.capital, alpha2code=country.alpha2code
+        )
         if country:
             weather = await self.get_weather(
                 LocationDTO(capital=country.capital, alpha2code=country.alpha2code)
             )
             currency_rates = await self.get_currency_rates(country.currencies)
+            news = await self.get_news(location_dto)
 
             return LocationInfoDTO(
                 location=country,
                 weather=weather,
                 currency_rates=currency_rates,
+                news=news,
             )
 
         return None
@@ -118,3 +125,12 @@ class Reader:
                 return True
 
         return False
+
+    async def get_news(self, location: LocationDTO) -> list[NewsInfoDTO] | None:
+        """
+        Получение новостей по стране.
+
+        :param location:
+        :return:
+        """
+        return await NewsCollector.read(location=location)
